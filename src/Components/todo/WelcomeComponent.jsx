cf9f0ee1ffb689b321b7889bebe4e9002e141f9e
Raman Kumar <kraman938@gmail.com>
2024-02-10 15:32:57 +0530

    Rest API fetched with axios(CORS Error)

--- src/Components/todo/WelcomeComponent.jsx
+++ src/Components/todo/WelcomeComponent.jsx
@@ -1,13 +1,36 @@
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function WelcomeComponent() {
+  function callHelloWorldRestApi() {
+    console.log("called");
+    axios
+    .get("http://localhost:8080/hello-rest")
+    .then((response) => SuccessfulResponse(response))
+    .catch((error) => ErrorResponse(error))
+    .finally(console.log('cleanup'))
+  }
+
+  function SuccessfulResponse(response) {
+    console.log(response)
+  }
+
+  function ErrorResponse(error) {
+    console.log('error' + error)
+  }
+
   const { username } = useParams();
   return (
     <div className="welcomeComponent">
       <h1>Welcome {username}</h1>
       <div>Personalized app for todos.</div>
+      <div>
+        <button className="btn btn-primary m-3" onClick={callHelloWorldRestApi}>
+          Hello World Api
+        </button>
+      </div>
     </div>
   );
 }
 
-export default WelcomeComponent
+export default WelcomeComponent;
