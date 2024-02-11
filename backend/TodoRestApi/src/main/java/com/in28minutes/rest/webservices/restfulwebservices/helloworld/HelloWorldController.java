4025eeddc01c303a45b5b38790280bd672c13c33
Raman Kumar <kraman938@gmail.com>
2024-02-11 17:25:56 +0530

    Spring Boot Hello world Bean to React Api

--- backend/TodoRestApi/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ backend/TodoRestApi/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -9,12 +9,12 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World"; 
+		return "Hello REST World";
 	}
 	
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World"); 
+		return new HelloWorldBean("Hello World Bean");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
--- src/Components/todo/WelcomeComponent.jsx
+++ src/Components/todo/WelcomeComponent.jsx
@@ -1,25 +1,35 @@
 import axios from "axios";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function WelcomeComponent() {
+  const { username } = useParams();
+  const [message, setMessage] = useState();
+
   function callHelloWorldRestApi() {
-    console.log("called");
+    // axios
+    //   .get("http://localhost:8080/hello-world")
+    //   .then((response) => SuccessfulResponse(response))
+    //   .catch((error) => ErrorResponse(error))
+    //   .finally(console.log("cleanup"));
+    
     axios
-    .get("http://localhost:8080/hello-world     ")
-    .then((response) => SuccessfulResponse(response))
-    .catch((error) => ErrorResponse(error))
-    .finally(console.log('cleanup'))
+      .get("http://localhost:8080/hello-world-bean")
+      .then((response) => SuccessfulResponse(response))
+      .catch((error) => ErrorResponse(error))
+      .finally(console.log("cleanup"));
+      
   }
 
   function SuccessfulResponse(response) {
-    console.log(response)
+    console.log(response);
+    setMessage(response.data.message)
   }
 
   function ErrorResponse(error) {
-    console.log('error' + error)
+    console.log("error" + error);
   }
 
-  const { username } = useParams();
   return (
     <div className="welcomeComponent">
       <h1>Welcome {username}</h1>
@@ -29,6 +39,7 @@ function WelcomeComponent() {
           Hello World Api
         </button>
       </div>
+      <div className="text-info">{ message }</div>
     </div>
   );
 }
