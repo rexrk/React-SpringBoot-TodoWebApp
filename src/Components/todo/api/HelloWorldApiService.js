3c2d9cc62f89df654e8522a39877796996a57d07
Raman Kumar <kraman938@gmail.com>
2024-02-25 21:33:32 +0530

    basic auth when logging in

--- src/Components/todo/api/HelloWorldApiService.js
+++ src/Components/todo/api/HelloWorldApiService.js
@@ -8,13 +8,18 @@ const apiClient = axios.create({
   baseURL: "http://localhost:8080",
 });
 
-export const retrieveHelloWorldBean = () =>
-  apiClient.get("/hello-world-bean");
+export const retrieveHelloWorldBean = () => apiClient.get("/hello-world-bean");
 
 export const retrieveHelloWorldPathVariable = (username) =>
   apiClient.get(`/hello-world/path-variable/${username}`, {
-    headers : {
-      Authorization : 'Basic cmV4cms6cmVz'
-    }
+    headers: {
+      Authorization: "Basic cmV4cms6cmVz",
+    },
   });
 
+export const executeBasicAuthenticationService = (token) =>
+  apiClient.get(`/basicauth`, {
+    headers: {
+      Authorization: token,
+    },
+  });
--- src/Components/todo/security/AuthContext.js
+++ src/Components/todo/security/AuthContext.js
@@ -1,30 +1,53 @@
 import { createContext, useContext, useState } from "react";
+import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export default function AuthProvider({ children }) {
   const [isAuthenticated, setAuthentication] = useState(false);
-  const [username, setUsername] = useState(null)
+  const [username, setUsername] = useState(null);
+  const [token, setToken] = useState(null);
 
-  function login(username, password) {
-    if (username === "rexrk" && password === "res") {
-      setAuthentication(true);
-      setUsername(username)
-      return true;
-    } else {
-      setAuthentication(false);
-      setUsername(null)
+  // function login(username, password) {
+  //   if (username === "rexrk" && password === "res") {
+  //     setAuthentication(true);
+  //     setUsername(username)
+  //     return true;
+  //   } else {
+  //     setAuthentication(false);
+  //     setUsername(null)
+  //     return false;
+  //   }
+  // }
+  async function login(username, password) {
+    const baToken = "Basic " + window.btoa(username + ":" + password);
+    try {
+      const response = await executeBasicAuthenticationService(baToken);
+
+      if (response.status == 200) {
+        setAuthentication(true);
+        setUsername(username);
+        setToken(baToken);
+        return true;
+      } else {
+        logout();
+        return false;
+      }
+    } catch (error) {
+      logout();
       return false;
     }
   }
 
   function logout() {
     setAuthentication(false);
+    setUsername(null);
+    setToken(null);
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, username}}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, username, token }}>
       {children}
     </AuthContext.Provider>
   );
